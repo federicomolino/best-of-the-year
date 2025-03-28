c503b87bed98a3a5ecf724e6aec06dc9b8402da4
federicomolino <federicomolino450@gmail.com>
2025-03-28 18:18:03 +0100

    add: ricerca film per id

--- best-of-the-year/src/main/java/com/bestofyear/best_of_the_year/Controller/Title.java
+++ best-of-the-year/src/main/java/com/bestofyear/best_of_the_year/Controller/Title.java
@@ -5,6 +5,7 @@ import com.bestofyear.best_of_the_year.entities.Songs;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -51,5 +52,20 @@ public class Title {
         List<Songs> songs = getBestSongs();
         model.addAttribute("song", songs);
         return "song";
+    }
+
+    //Ricerco per id
+    @GetMapping("/movies/{id}")
+    public String MovieID(@PathVariable("id") int id, Model model){
+        List<Movie> films = getBestMovies();
+        Movie trovato = null;
+        for (Movie f : films){
+            if (f.getId() == id){
+                model.addAttribute("film", f);
+                trovato = f;
+                break;
+            }
+        }
+        return "searchID";
     }
 }
